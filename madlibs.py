4cd6fb655fffc6d11cc4b31f29caca888206a487
JaycobJuan <jparham1406@bellsouth.net>
2025-02-06 23:08:24 -0500

    init commit

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adjective = input("Adjective: ")
+verbOne = input("Verb: ")
+verbTwo = input("Verb: ")
+famous = input("Famous person: ")
+print(f"Computer programming is so {adjective}! It makes me so excited all"
+      f" the time because I love to {verbOne}. Stay hydrated and {verbTwo} "
+      f"like you are {famous}!")
